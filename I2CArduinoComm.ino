f5344872eb5adea0f5f600400387abcce4541e93
Morten Kromberg <mkrom@dyalog.com>
2013-07-22 18:58:45 +0200

    ArdCom.dyalog is a Dyalog APL wrapper for the I2CArduinoCom program. See Examples/DyaBot.dyalog for an example of a robot control program which uses it.

--- I2CArduinoComm.ino
+++ I2CArduinoComm.ino
@@ -1,132 +1,285 @@
 /*
  * I2C to Arduino Mino Pro Communication Layer
- * Converted from the quick2wire trackbot by Liam Flanagan
- * For more information see https://github.com/quick2wire/trackbot
- *
  * for Arduino mini pro pinouts see http://arduino.cc/en/uploads/Main/Arduino-Pro-Mini-schematic.pdf
  * for motor control pin names see https://content.solarbotics.com/products/datasheets/solarbotics_l298_compact_motor_driver_kit.pdf
  *
  */
 
 #include <Wire.h>
+#include <Servo.h>
 
-// Lights
-const int onboardLED = 13;
-const int I2C_ADDRESS = 4;
-// Sensors
-int ReadPin = A3;
-int readType = 0; // 0 for analog 1 for digital
+const boolean DEBUG = true;
+boolean newDataAvailable = false; // Is there a response ready?
+boolean RESULT; // Did the last request produce a "result"?
 
-void loop() { }  //  Required for Arduino main function
+const int I2C_ADDRESS =  4;
+const int NumPins   =   20; // Arduino Mini
+const int MaxBuf    =   32;
+const int MaxServos =    1;
+const int MaxLog    = 1024;
 
-/*
- * Light helper functions
- */
-void flashOnboardLED()
-{
-	digitalWrite(onboardLED, HIGH);
-	delay(800);
-	digitalWrite(onboardLED, LOW);
+Servo Servos[MaxServos]; // Up to 6 servos
+int UsedServos = 0;
+
+byte INPUTPINS[NumPins]; // List of input pins
+int NumInputs = 0; // Count the number of input pins
+
+
+byte OUTDATA[MaxBuf];
+byte RESULTDATA[MaxBuf]; 
+char LOGTXT[MaxLog]; int logend = 0; int logptr = 0; byte LOGBUF[MaxBuf]; // Debug log
+char PINTYPE[NumPins]; //Pin Type (blank, or one of ADSadp)
+byte PININFO[NumPins]; //Extra control data (used for pulse pairs)
+
+void logstr(char text[32])
+{ logend = logend + sprintf(&LOGTXT[logend],"%s", text); }
+void logint(int i)
+{ logend = logend + sprintf(&LOGTXT[logend],"%d", i); }
+void logchar(char c)
+{ LOGTXT[logend] = c; logend++; }
+
+void resetall(void)
+{ 
+  for (int i=0; i < NumPins; i++) 
+  {  PINTYPE[i] = ' ';
+     PININFO[i] = 0;
+  }
+  NumInputs = 0;
+  newDataAvailable = false;
 }
-void flashForStartup()
+
+void registerinput(int pin)
 {
-	pinMode(onboardLED, OUTPUT);
-	flashOnboardLED();
-	delay(200);
-	flashOnboardLED();
+  pinMode(pin, INPUT);
+  INPUTPINS[NumInputs]=pin;
+  NumInputs++;
+}
+void loop() { 
+    // Collect input data and store it in OUTDATA 
+    int outvalue;
+    byte bufptr=1;     
+    for (int i=0 ; i < NumInputs; i++) // Loop through all input pins
+    {
+      int pin = INPUTPINS[i];
+      switch (PINTYPE[pin])
+      {
+      case 'a': // Analog Input
+        { 
+          outvalue = analogRead(pin); // AnalogInputPins[pin]); 
+          break;
+        }
+      case 'd': // Digital Input
+        { 
+          outvalue = digitalRead(pin); 
+          break;
+        }
+      case 'p': // Pulse input
+        { 
+          byte trigger =PININFO[pin];
+          if (trigger > 0) // Trigger Pulse =>Pull pin high for 10us
+          {
+            digitalWrite(trigger, LOW); 
+            delayMicroseconds(2);
+            digitalWrite(trigger, HIGH); 
+            delayMicroseconds(10);
+            digitalWrite(trigger, LOW);
+          }
+          long ov = pulseIn(pin, HIGH); // Timeout 10ms
+          outvalue = (int) ov;
+          break;
+        }
+      }    
+      byte * bytePointer = (byte*)&outvalue; 
+      OUTDATA[bufptr] = bytePointer[1]; bufptr++; // Store MSB
+      OUTDATA[bufptr] = bytePointer[0]; bufptr++; // Store LSB
+      // int len = sprintf(&OUTDATA[bufptr]," %d", outvalue);
+      // bufptr = bufptr + len;
+    }
+    OUTDATA[0] = bufptr; // First byte is length
 }
 
-
-/*
- * Event callback function
- * This executes whenever data is received from master.
- * It is registered as an event in the setup function.
- */
-const int PACKET_SIZE = 3;	// bytes
 void ReceiveEvent(int numBytes)
 {
-  // Serial.println("Receive event");
-	// flashOnboardLED();	// Let user know we have triggered an event
-	
-	if (0 == (numBytes % PACKET_SIZE))
-	{
-		int numPackets = numBytes / PACKET_SIZE;
-		
-		for (int i=0; i < numPackets; i++)
-		{
-			char	function = Wire.read();
-			byte	pin      = Wire.read(),
-				value    = Wire.read();
+  RESULT = false;
+  logptr = 0; logend = 0; // Reset LOG
+  byte bufptr = 1;  // Pointer into OUTDATA while preparing output
+  int ptr = 0;      // Pointer into input data
+
+  byte n = Wire.read(); // Command length
+
+  if (n == numBytes-1)
+  {
+    char function = Wire.read(); 
+    ptr++;
+    switch (function)
+    {
+    case 'R': // Reset
+      {        
+        resetall(); // for (int i=0; i < NumPins; i++) PINTYPE[i] = ' ';
+        if (DEBUG == true) {logstr("R: "); logint(NumPins); logstr(" pins reset.\n");} 
+        break;
+      } 
+
+    case 'I': // Identify
+      {
+        // Builds OUTDATA buffer for next I2C READ event
+        if (DEBUG == true) logstr("I: ArdCom version 0.2:\n");
+        RESULTDATA[1]='0'; RESULTDATA[2]='2'; // Ardcom Major & Minor Version numbers
+        bufptr=3; 
+        int pins=0;
         
-			switch (function)
-			{
-				case 'A':
-				{
-					pinMode(pin, OUTPUT);
-					analogWrite(pin,value);
-					break;
-				}
-				case 'a':
-				{
-					pinMode(pin, INPUT);
-					ReadPin=pin;
-					readType = 0;
-					break;
-				}
-				case 'D':
-				{            
-					pinMode(pin, OUTPUT);
-					digitalWrite(pin,value?HIGH:LOW);
-					break;
-				}
-				case 'd':
-				{
-					pinMode(pin, INPUT);
-					ReadPin=pin;
-					readType = 1;
-					break;
-				}
-			}
-		}
-	}
-	else
-	{
-		// Error
-		// Packet(s) not of a recognised size
-		for (int i=0; i < numBytes; i++)
-		{
-			// Clear buffer on the master
-			Wire.read();
-		}
-	}
+        for (int pin=0 ; pin < NumPins; pin++) // Document each pin
+        {
+          char pintype = PINTYPE[pin];
+          if (pintype != ' ') // && bufptr < 32) // Defined pins - but don't overwrite buffer
+          {
+            RESULTDATA[bufptr] = pin; bufptr++;
+            RESULTDATA[bufptr] = pintype; bufptr++;
+            pins++;
+           if (DEBUG == true) {logint(pin); logstr("="); logchar(pintype); logstr(",");}
+          }
+        }
+        RESULTDATA[0]=bufptr-1;
+        RESULT = true; // The function put data in OUTDATA
+        if (DEBUG == true) logstr("\n");
+        break;
+      }
+
+    case 'S':  // Setup
+      { 
+        if (DEBUG == true) logstr("S:\n");
+        int pins = 0;
+        while (ptr < n) // Triplets of pin,type,info
+        {
+          byte pin = Wire.read(); 
+          char pintype = Wire.read(); 
+          PININFO[pin] = Wire.read(); 
+          PINTYPE[pin] = pintype;
+          if (DEBUG == true) {logstr("Pin "); logint(pin); logstr(" = "); logchar(pintype);}
+          ptr=ptr+3; 
+          switch (pintype)
+          {
+          case 'S': // Servo
+            {
+              Servos[UsedServos].attach(pin);
+              PININFO[pin]=UsedServos;
+              UsedServos++;
+              pinMode(pin, OUTPUT);
+              if (DEBUG == true) {logstr (" - servo #"); logint(UsedServos); logstr(" attached.");}
+              break;
+            }
+          case 'p': // Pulse Input
+            { 
+              registerinput(pin);
+              pinMode(PININFO[pin], OUTPUT); // Trigger Pin is output
+              if (DEBUG == true) {logstr (" - trigger pin is #"); logint(PININFO[pin]); logstr(".");}
+              break;
+            }
+          case 'd':
+            { 
+              registerinput(pin);
+              break; 
+            }
+          case 'a':
+            { 
+              registerinput(pin);
+              break; 
+            }
+          case 'A':
+            { 
+              pinMode(pin, OUTPUT); 
+              break; 
+            }
+          case 'D':
+            { 
+              pinMode(pin, OUTPUT); 
+              break; 
+            }
+          }
+           if (DEBUG == true) logstr("\n.");
+           pins++;
+        } // While (each pin)
+        if (DEBUG == true) 
+          {logint(pins); logstr(" pins defined, of which ");
+           logint(NumInputs); logstr(" inputs.\n");}        
+        break;
+      }
+    case 'W':  // Write      
+      { 
+        while (ptr < numBytes)
+        { //Pairs of pin, value
+          byte pin = Wire.read(); 
+          byte value = Wire.read(); 
+          ptr=ptr+2;
+          switch(PINTYPE[pin])
+          {
+          case 'A':
+            { 
+              analogWrite(pin,value); 
+              break; 
+            }
+          case 'D':
+            { 
+              digitalWrite(pin,value?HIGH:LOW); 
+              break; 
+            }
+          case 'S':
+            { 
+              Servos[PININFO[pin]].write(value); 
+              break; 
+            }        
+          }     
+        } 
+        break;
+      }
+     default:
+     {logstr("Invalid command received: "); logchar(function); logstr("\n");}
+    }
+  }
+  else
+  {
+    do {
+      Wire.read(); ptr++; // Error: Numbytes does not match - just throw it away
+    } while (ptr < numBytes);  
+     logstr("Ill formatted command.\n");
+}
 }
 
 void onI2CRequest()
-{
-  // Serial.println("Send event");
-  char buf[16];
-  int PinVal;
-  int len;
-	if (0 == readType ) {
-	  PinVal = analogRead(ReadPin);
-	  len = sprintf(&buf[1],"a%d:%d;", ReadPin, PinVal);
-	} else {
-	  PinVal = digitalRead(ReadPin);
-	  len = sprintf(&buf[1],"d%d:%d;", ReadPin, PinVal);
-	}
-  buf[0] = len;
-  // Serial.println(buf);
-  Wire.write(buf);
+{ 
+  byte sent;
+
+  // First, see if there is diagnostic output to be sent 
+  if (logend > logptr)
+  {
+   LOGBUF[1]=254; // Signal to recipient that this is diagnostic output (MSB of data cannot be 254)
+   int i = logend - logptr; if (i > 30) i = 30;  // How much to send? We need 2 bytes for envelope.     
+   memcpy(&LOGBUF[2], &LOGTXT[logptr], i);
+   logptr = logptr + i;
+   LOGBUF[0] = i+1;
+   sent = Wire.write(LOGBUF,i+2);
+   return;
+  }
+
+  // Next: Is there a pending result from the last command?
+  if (RESULT == true) 
+  {
+    sent = Wire.write(RESULTDATA,1+RESULTDATA[0]); 
+    RESULT = false;
+  }
+  else // Return the latest sample data
+  {
+    sent = Wire.write(OUTDATA,1+OUTDATA[0]); 
+    // OUTDATA[OUTDATA[0]]=0;
+    // sent = Wire.write(OUTDATA); 
+  }
 }
 
 void setup()
 {
-	Serial.begin(9600);
-
-	Wire.begin(I2C_ADDRESS);
-	Wire.onRequest(onI2CRequest);
-	Wire.onReceive(ReceiveEvent);
-
-	
-	flashForStartup();
+  resetall(); 
+  Wire.begin(I2C_ADDRESS);
+  Wire.onRequest(onI2CRequest);
+  Wire.onReceive(ReceiveEvent);
 }
+
